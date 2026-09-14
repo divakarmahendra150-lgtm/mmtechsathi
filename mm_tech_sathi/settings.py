6568bdec26223b5c0a1739bac1f8c96c9644f064
Mahendra Ahirwar <divakarmahendra150@gmail.com>
2026-09-14 19:54:01 +0530

    Update settings.py

--- mm_tech_sathi/settings.py
+++ mm_tech_sathi/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'SecretKey'
 DEBUG = 'On'
 
 ALLOWED_HOSTS = [
-    "mmtechsathi.onrender.com"
+    "mmtechsathi.onrender.com",
+    "localhost",
+    "127.0.0.1",
 ]
 
 CORS_ALLOWED_ORIGINS = [
